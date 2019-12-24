88956fd9bbb058ab38b12acb92d1c70b17cd4cde
farooque.mustafa <farooque.mustafa@alterway.fr>
2019-12-24 14:06:42 +0100

    two layers with yarn

--- src/index.js
+++ src/index.js
@@ -12,11 +12,32 @@ const systemMetrics = {
 		max : 128,
 		current : 32
 	},
+	ram2 : {
+		label : "RAM",
+		unit : "GB",
+		max : 128,
+		current : 32
+	},
 	hdd : {
 		label : "HDD",
 		unit : "GB",
 		max : 1024,
 		current : 150
+	},
+	bandwidth : {
+		label : "BW",
+		unit : "MB / seconds",
+		max : 10240,
+		current : 1500
+	}
+}
+
+const qoeMetrics = {
+	hdd : {
+		label : "RAM",
+		unit : "GB",
+		max : 128,
+		current : 32
 	},
 	bandwidth : {
 		label : "BW",
@@ -29,93 +50,71 @@ const systemMetrics = {
 		unit : "MB / seconds",
 		max : 10240,
 		current : 1500
+	},
+	ram : {
+		label : "RAM",
+		unit : "GB",
+		max : 128,
+		current : 32
+	},
+	io : {
+		label : "IO",
+		unit : "IO / seconds",
+		max : 10240,
+		current : 1500
 	}
 }
-/*
-const dataValue = [];
-const dataTitle = [];
-for ( const metric in systemMetrics) {
-    const value = systemMetrics[metric].current / systemMetrics[metric].max;
-	dataValue.push(value);
-	dataTitle.push(metric);
-};
-*/
-
-const dataValue = Object.values(systemMetrics).map(e => e.current / e.max)
-const dataTitle = Object.keys(systemMetrics)
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 5000 );
-camera.position.set( 0, 20, 100 );
-camera.lookAt( 0, 0, 0 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const labelRenderer = new THREE.CSS2DRenderer();
-labelRenderer.setSize( window.innerWidth, window.innerHeight );
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = 0;
-document.body.appendChild( labelRenderer.domElement );
-
-const controls = new THREE.OrbitControls(camera, labelRenderer.domElement);
-//controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
-//controls.target = new THREE.Vector3(.5, .5, .5);
-			
-const scene = new THREE.Scene();
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-const material = new THREE.LineDashedMaterial( {
-	color: 0xffffff,
-	linewidth: 3,
-	scale: 1,
-	dashSize: 3,
-	gapSize: 1,
-} );
 
 class SimpleLine extends THREE.Line {
-	constructor(value1, value2, material) {
+	constructor(value1, value2, material2) {
 		const geometry = new THREE.Geometry();
 		geometry.vertices.push(new THREE.Vector3(...value1) );
 		geometry.vertices.push(new THREE.Vector3(...value2) );
-		super(geometry, material)
+		super(geometry, material2)
 	}
-	}
-
-const b = [];
-
-
-
-labelRenderer.render( scene, camera );
-
-for (let i=0; i< dataValue.length; i++){
-	const pointValue = new SimpleLine([dataValue[i]*50,0,0], [0,0,0] , material);
-	pointValue.rotateZ(i*Math.PI*2/dataValue.length);
-	scene.add(pointValue);
-	scene.updateMatrixWorld();
-	const points = pointValue.geometry.vertices[0].clone();
-	points.applyMatrix4(pointValue.matrixWorld);
-	b.push(points);
 }
 
-for(let i = 0; i < b.length -1; i++) {
-	//console.log(b[i], b[i+1])
-	const autoLines = new SimpleLine(b[i].toArray(), b[i+1].toArray(), material);
-	scene.add(autoLines);
+const dataValueSystemMetrics = Object.values(systemMetrics).map(e => e.current / e.max);
+const dataTitleSystemMetrics = Object.keys(systemMetrics);
+const dataValueQoeMetrics = Object.values(qoeMetrics).map(e => e.current / e.max);
+const dataTitleQoeMetrics = Object.keys(qoeMetrics);
 
-	const label = createLabel(dataTitle[i], b[i]);
-	scene.add(label); 
+const { scene, labelRenderer, controls, renderer, camera } = initScene();
+
+const material1 = createLineMaterial(0xffffff);
+const material2 = createLineMaterial(0x000000);
+
+const plane1points = [];
+const plane2points = [];
+
+loopingOverLayers(plane1points, dataValueSystemMetrics, 10);
+loopingOverLayers(plane2points, dataValueQoeMetrics, -10);
+
+const pointsCount = plane1points.length;
+for(let i = 0; i < pointsCount; i++) {
+	const planeOneLines = new SimpleLine(plane1points[i].toArray(), plane1points[(i+1)  % pointsCount].toArray(), material1);
+	scene.add(planeOneLines);
+
+	const lineAddingBothPlanes = new SimpleLine(plane1points[i].toArray(), plane2points[i].toArray(), material1);
+	scene.add(lineAddingBothPlanes);
+
+	const labelForPlane1 = createLabel(dataTitleSystemMetrics[i], plane1points[i]);
+	scene.add(labelForPlane1); 
+
+	const labelForPlane2 = createLabel(dataTitleQoeMetrics[i], plane2points[i]);
+	scene.add(labelForPlane2); 
+
+	const planeTwoLines = new SimpleLine(plane2points[i].toArray(), plane2points[(i+1) % pointsCount].toArray(), material1);
+	scene.add(planeTwoLines);
 }
 
-const autoLines1 = new SimpleLine(b[b.length - 1].toArray(), b[0].toArray(), material);
-scene.add(autoLines1);
 
-const label = createLabel(dataTitle[dataTitle.length -1], b[b.length - 1]);
-scene.add(label);
-	
+//creates labels for all mertics and displays it on their position.
+/**
+ * 
+ * @param {returns labels for your parameters and displays on your 3D plane} textContent 
+ * @param {connects x&y coordinates to form lines between points} vector3 
+ */
 function createLabel(textContent, vector3) {
 	const labelDiv = document.createElement( 'div' );
 	labelDiv.className = 'label';
@@ -134,29 +133,78 @@ function render() {
 	labelRenderer.render( scene, camera );
 	
 	requestAnimationFrame(render);
-	}
+}
 
 render();
 
 
-/*
-// game logic
-const update function ( ){
+function createLineMaterial(color) {
+	return new THREE.LineDashedMaterial( {
+		color,
+		linewidth: 3,
+		scale: 1,
+		dashSize: 3,
+		gapSize: 1,
+	} );
+}
 
-};
+function initScene() {
+	const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 5000 );
+	camera.position.set( 0, 20, 100 );
+	
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+	
+	window.addEventListener( 'resize', function() {
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		renderer.setSize( width, height);
+		labelRenderer.setSize( width, height);
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix( );
+	});
+	
+	const labelRenderer = new THREE.CSS2DRenderer();
+	labelRenderer.setSize( window.innerWidth, window.innerHeight );
+	labelRenderer.domElement.style.position = 'absolute';
+	labelRenderer.domElement.style.top = 0;
+	document.body.appendChild( labelRenderer.domElement );
+	
+	const controls = new THREE.OrbitControls(camera, labelRenderer.domElement);
+	//controls.autoRotate = true;
+	controls.autoRotateSpeed = 5;
+	//controls.target = new THREE.Vector3(.5, .5, .5);
+				
+	const scene = new THREE.Scene();
+	const axesHelper = new THREE.AxesHelper(5);
+	scene.add(axesHelper);
 
-// draw scene
-const render = function( ) {
-	renderer.render( scene, camera );
-};
+	labelRenderer.render( scene, camera );
 
-// run game loop (update, render, repeat)
-const gameLoop = function ( ) {
-	requestAnimationFrame ( gameLoop );
+	return { scene, labelRenderer, controls, renderer, camera };
+}
 
-	update( );
-	render( );
-};
-
-gameLoop();
-*/
+function loopingOverLayers (planepoints, planeMetrics, zplaneValues) {
+	for (let i=0; i< planeMetrics.length; i++){
+		pallindrome(planepoints, planeMetrics, i, zplaneValues)
+	}
+}
+/**
+ * Draw a palindrome
+ * 
+ * @param {Array} coordinates list of coordinates to reuse
+ * @param {Array} metricValue list metric of a layer
+ * @param {number} i iteration over the array's index
+ * @param {number} zplane position on the z axis of the palindrome
+ */
+function pallindrome(coordinates, metricValue, i, zplane) {
+	const pointValue = new SimpleLine([metricValue[i]*50,0,zplane], [0,0,zplane] , material2);
+	pointValue.rotateZ(i*Math.PI*2/metricValue.length);
+	scene.add(pointValue);
+	scene.updateMatrixWorld();
+	const points = pointValue.geometry.vertices[0].clone();
+	points.applyMatrix4(pointValue.matrixWorld);
+	coordinates.push(points);
+}
