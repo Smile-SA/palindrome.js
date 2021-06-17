cfd9e17e525982867811cc26131fdba7bdd27bf1
koku-ulrich.gblokpo <koku-ulrich.gblokpo@alterway.fr>
2021-06-18 00:02:42 +0200

    implementation of controls and updating of the storybook 18 juin 2021 / 00:02

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { debugTwoLayersThreePoints } from './src/data_structures_examples/debug_TwoLayersThreePoints';
 import {defaultControl} from "./controls/default_controls";
-import {createPalindrome} from "./controls/createPalindrome";
+import {getPalindrome} from "./controls/getPalindrome";
 export default {
     title: 'Use Cases/Palindrome/Data Center example',
     argTypes: defaultControl(),
 };
-const twoLayersThreePoints = createPalindrome.bind({});
+const twoLayersThreePoints = getPalindrome.bind({});
 twoLayersThreePoints.args = {
     TextStyle:1,
     displayUnits: true,
@@ -28,9 +28,9 @@ twoLayersThreePoints.args = {
     gridDivisions: 100,
     metricMagnifier: 10,
     layerStatusControl: true,
-    layerMidColor: '#DFDF0B',
-    mainAppColor: '#00FF06',
-    subAppColor: '#9FC5E8',
+    layerMidColor: '#FF2C00',
+    mainAppColor: '#FF2C00',
+    subAppColor: '#FFFFFF',
     statusRangelow: 0,
     statusRangemed: 30,
     statusRangehigh: 60,
@@ -46,50 +46,3 @@ twoLayersThreePoints.args = {
     zplaneMultilayer: -20,
     data: debugTwoLayersThreePoints(),
 };
-
-    const config = {
-        mockupData: true,
-        displayArea: 'palindrome',
-    	palindromeSize : 3,
-        displaySides : true,
-        displayMode: 'dynamic',
-        displayLayers : true,
-        displayLayersLines : true,
-        layerDisplayMode: 'dynamic',
-        displayLabels : true,
-        displayLabelsAll : true,
-        displayGrid: true,
-        gridSize: 100,
-        gridDivisions: 100,
-        metricMagnifier: 10,
-        layerStatusControl: true,
-        layerMidColor:'#FF2C00',
-        mainAppColor:'#FFCC00',
-        subAppColor:'#FFFFFF',
-        statusRange: {
-            low: '0',
-            med: '30',
-            high: '60'
-        },
-        statusColor: {
-            low: '#9FC5E8',
-            med: '#00FF00',
-            high: '#FF0000'
-        },
-        line: {
-            lineColor: '#000000',
-            lineOpacity: 1,
-            lineTranparency: 0.5,
-            lineWidth: 3
-        },
-        zplane: {
-            zplaneInitial: 0,
-            zplaneHeight: 40,
-            zplaneMultilayer: -20
-        },
-        data: debugTwoLayersThreePoints()
-    };
-
-    const container = document.getElementById('palindrome');
-    palindrome(container, JSON.parse(JSON.stringify(config)));
-
