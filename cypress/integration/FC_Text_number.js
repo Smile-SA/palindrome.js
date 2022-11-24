1eb6451816c12f7617be21bf417673316a31c66a
saifuddin.mohammad <saifuddin.mohammad@alterway.fr>
2022-11-24 11:55:47 +0100

    Updated Functional tests

--- cypress/integration/FC_Text_number.js
+++ cypress/integration/FC_Text_number.js
@@ -1,7 +1,7 @@
 //This file integrates defaultControls storybook into cypress testing environment
 /// <reference types="cypress" />
 
-import { defaultControls, defaultValues } from '/home/saif-alterway/palindrome/palindrome.js/stories/controls/defaultControls.js';
+import { defaultControls, defaultValues } from '/stories/controls/defaultControls.js';
 
 var controls = Array.from(Object.keys(defaultControls()))
 
@@ -13,8 +13,7 @@ describe ('Functional testing for Controls ', function () {
   controls.forEach(element => {
     var ele = "#control-" + element
     if (defaultControls()[element].control == 'number' | 
-        defaultControls()[element].type == 'text' | 
-        defaultControls()[element].control == 'color') {
+        defaultControls()[element].type == 'text' ) {
       cy.get(ele).wait(1000).should('have.value', defaultValues()[element])
     }
   })
