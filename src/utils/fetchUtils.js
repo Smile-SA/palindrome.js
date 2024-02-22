b66e86f60e36ce31201b778693a3ec372d1eec9f
mohamed.yacoubi <mohamed.yacoubi@smile.fr>
2024-02-22 14:34:39 +0100

    issue-170 improve remote servers based usecases and usage documentation.

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -53,7 +53,7 @@ export const createInputUrlModal = (parentElement) => {
     modalContent.appendChild(span);
 
     const title = document.createElement('p');
-    title.innerHTML = 'Oops... It looks like there is something wrong. Please fill out your remote data source URL:';
+    title.innerHTML = '... Something wrong with the server call. Please fill out your remote data source URL:';
     title.style = 'margin-left: 20px;';
     modalContent.appendChild(title);
 
