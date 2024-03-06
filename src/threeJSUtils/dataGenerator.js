2e36db8330ceaf477b44c3ec5c9404b105ba1e6f
Mohamed Ali Yacoubi <mohamed.yacoubi@smile.fr>
2024-03-06 16:05:47 +0100

    issue-55 use case: maslow's pyramid of needs
    fixing mockup data for Pyramid of Maslow's

--- src/threeJSUtils/dataGenerator.js
+++ src/threeJSUtils/dataGenerator.js
@@ -1,3 +1,4 @@
+import { getMetricMax, getMetricMin } from "../utils/metricsUtils2D";
 /**
  * Generates random data for mockupDataConfig
  * @param {*} model 
@@ -13,9 +14,9 @@ export function* dataGenerator(model) {
                 let update = ((Math.random() - .5) * 2 * (max - min) / 100);
                 let currentKey = "current";
                 if (Object.values(layer.layer)[0]?.layerSize) {
-                    update = ((Math.random() - .5) * 2 * (_max - _min) / 100);
+                    update = ((Math.random() - .5) * 2 * (getMetricMax(metric) - getMetricMin(metric))/100);
                     currentKey = "_current";
-                    metric[currentKey] = Math.max(Math.min(_current + update, _max), _min);
+                    metric[currentKey] = Math.max(Math.min(_current + update, getMetricMax(metric)), getMetricMin(metric));
                 } else {
                     metric[currentKey] = Math.max(Math.min(current + update, max), min);
                 }
