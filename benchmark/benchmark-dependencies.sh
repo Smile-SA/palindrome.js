ee3fd3630ee8b333c226f85a9cab2e49a05f8e77
mohamed.yacoubi <mohamed.yacoubi@smile.fr>
2024-04-16 11:54:08 +0200

    ci: moving yarn, node, and dependencies installation inside .gitlab-ci.yml file

--- benchmark/benchmark-dependencies.sh
+++ benchmark/benchmark-dependencies.sh
@@ -43,26 +43,6 @@ if [[ -z "$(command -v jq)" ]]; then
     jq --version
 fi
 
-# Install curl if not already installed
-if [[ -z "$(command -v curl)" ]]; then
-    ${cmd_prefix} apt install -y sudo curl
-    curl --version
-fi
-
-# Install Node.js LTS version if not already installed
-if [[ -z "$(command -v node)" ]]; then
-    curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-    ${cmd_prefix} apt install -y nodejs
-    node -v
-fi
-
-# Install Yarn if not already installed
-if [[ -z "$(command -v yarn)" ]]; then
-    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    ${cmd_prefix} apt update && sudo apt install -y yarn
-fi
-
 # Install Firefox if not already installed
 if [[ -z "$(command -v firefox)" ]]; then
     ${cmd_prefix} apt install -y firefox
