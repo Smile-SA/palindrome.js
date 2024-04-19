3829b9e5df1c7a8ab61ac7c4461db88534986130
jonriv <jonathan.rivalan@smile.fr>
2024-04-19 11:39:43 +0200

    fix: variables declarations and wrapping + wip: early functions structure

--- benchmark-eval.sh
+++ benchmark-eval.sh
@@ -3,144 +3,186 @@
 # Set up benchmark parameters
 source ./benchmark.env.sh
 
-# Function to compare floating-point numbers
+# Compare floating-point numbers
 compare_float() {
     awk -v n1="$1" -v n2="$2" 'BEGIN {if (n1 < n2) exit 0; exit 1}'
 }
 
-# Function to calculate the median of an array
+# Calculate the median of an array
 calculate_median() {
-    local array=("$@")
-    local array_length=${#array[@]}
+    declare -a array
+    declare -a sorted_array
+    local array_length
     local array_odd_or_even
+    local median
+    local middle
+    local middle_first
+    local middle_second
+
+    array=(${@})
+    array_length="${#array[@]}"
     array_odd_or_even=$(( array_length % 2 ))
     sorted_array=($(for i in "${array[@]}"; do echo $i; done | sort))
+
     if [[ "${array_odd_or_even}" -eq 0 ]]; then
         # For an even-sized array, calculate the average of the middle two elements
-        local middle_first=$((array_length / 2 - 1))
-        local middle_second=$((middle_first + 1))
-        median=$(( (sorted_array[middle_first] + sorted_array[middle_second]) / 2 ))
+        middle_first=$(( array_length / 2 - 1 ))
+        middle_second=$(( middle_first + 1 ))
+        median=$(( ( sorted_array[middle_first] + sorted_array[middle_second] ) / 2 ))
     else
         # For an odd-sized array, the median is the middle element
-        local middle=$((array_length / 2))
-        median=${sorted_array[middle]}
+        middle=$(( array_length / 2 ))
+        median="${sorted_array[middle]}"
     fi
 
-    echo "$median"
+    echo "${median}"
 }
 
 # Function to launch benchmark and retrieve json results
 launch_benchmark_and_get_results() {
+    local file_content
+    declare -a values
+
     rm -f "${HOME}/Downloads/${PALINDROME_BENCH_OUTPUT}"*
     pkill firefox
     bash ./benchmark.sh > /dev/null
     file_content=$(cat "${HOME}/Downloads/${PALINDROME_BENCH_OUTPUT}.results")
 
-    values=()
-    values+=("$(echo "$file_content" | jq -r '.Basic_version_results."Average FPS rendered"')")
-    values+=("$(echo "$file_content" | jq -r '."Web workers_version_results"."Average FPS rendered"')")
-    values+=("$(echo "$file_content" | jq -r '.Basic_version_results."Average Milliseconds needed to render a frame"')")
-    values+=("$(echo "$file_content" | jq -r '."Web workers_version_results"."Average Milliseconds needed to render a frame"')")
+    values+=("$(echo "${file_content}" | jq -r '.Basic_version_results."Average FPS rendered"')")
+    values+=("$(echo "${file_content}" | jq -r '."Web workers_version_results"."Average FPS rendered"')")
+    values+=("$(echo "${file_content}" | jq -r '.Basic_version_results."Average Milliseconds needed to render a frame"')")
+    values+=("$(echo "${file_content}" | jq -r '."Web workers_version_results"."Average Milliseconds needed to render a frame"')")
     echo "${values[@]}"
 }
 
-# outputFile="${HOME}/Downloads/${PALINDROME_BENCH_OUTPUT}.results"
-fps_workers=()
-ms_workers=()
-fps_basic=()
-ms_basic=()
-
 # Getting expected values from remote data source or computing local median
-if [[ "$PALINDROME_BENCH_EXPECTED_VALUES_SOURCE" == "remote" ]]; then
-    remote_values=$(curl $PALINDROME_BENCH_EXPECTED_VALUES_REMOTE_SOURCE_URL -H "Accept: application/json")
-    dynamic_fps_workers=$(echo "$remote_values" | jq -r '.workersFps')
-    dynamic_fps_basic=$(echo "$remote_values" | jq -r '.basicFps')
-    dynamic_ms_workers=$(echo "$remote_values" | jq -r '.workersMs')
-    dynamic_ms_basic=$(echo "$remote_values" | jq -r '.basicMs')
-elif [[ "$PALINDROME_BENCH_EXPECTED_VALUES_SOURCE" == "local" ]]; then
-    if [[ "$PALINDROME_BENCH_RECALCULATE_MEDIAN" == false ]]; then
-        if [[ -f "$PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT" ]]; then
-            median_values=$(cat "$PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT")
-            dynamic_fps_workers=$(echo "$median_values" | jq -r '.workersFps')
-            dynamic_fps_basic=$(echo "$median_values" | jq -r '.basicFps')
-            dynamic_ms_workers=$(echo "$median_values" | jq -r '.workersMs')
-            dynamic_ms_basic=$(echo "$median_values" | jq -r '.basicMs')
-            rm -f median.out
-        else
-            echo "[ERROR] Median file is not found, recomputing median again."
+# Inherit: $remote_values, $median_values, $dynamic_fps_workers, $dynamic_fps_basic, $dynamic_ms_workers, $dynamic_ms_basic
+# $result_array, $fps_workers, $ms_workers, $fps_basic, $ms_basic
+eval_benchmark_get_median(){
+    if [[ "${PALINDROME_BENCH_EXPECTED_VALUES_SOURCE}" == "remote" ]]; then
+        remote_values=$(curl ${PALINDROME_BENCH_EXPECTED_VALUES_REMOTE_SOURCE_URL} -H "Accept: application/json")
+        dynamic_fps_workers=$(echo "${remote_values}" | jq -r '.workersFps')
+        dynamic_fps_basic=$(echo "${remote_values}" | jq -r '.basicFps')
+        dynamic_ms_workers=$(echo "${remote_values}" | jq -r '.workersMs')
+        dynamic_ms_basic=$(echo "${remote_values}" | jq -r '.basicMs')
+    elif [[ "${PALINDROME_BENCH_EXPECTED_VALUES_SOURCE}" == "local" ]]; then
+        if [[ "${PALINDROME_BENCH_RECALCULATE_MEDIAN}" == false ]]; then
+            if [[ -f "${PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT}" ]]; then
+                median_values=$(cat "${PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT}")
+                dynamic_fps_workers=$(echo "${median_values}" | jq -r '.workersFps')
+                dynamic_fps_basic=$(echo "${median_values}" | jq -r '.basicFps')
+                dynamic_ms_workers=$(echo "${median_values}" | jq -r '.workersMs')
+                dynamic_ms_basic=$(echo "${median_values}" | jq -r '.basicMs')
+                rm -f median.out
+            else
+                echo "[ERROR] Median file is not found, recomputing median again."
+            fi
+        fi
+
+        if [[ "${PALINDROME_BENCH_RECALCULATE_MEDIAN}" == true ]] || [[ ! -f "${PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT}" ]]; then
+            for i in $(seq 1 ${PALINDROME_BENCH_MEDIAN_ITERATIONS}); do
+                result_array=($(launch_benchmark_and_get_results))
+                fps_workers+=("${result_array[0]}")
+                ms_workers+=("${result_array[1]}")
+                fps_basic+=("${result_array[2]}")
+                ms_basic+=("${result_array[3]}")
+                echo "${result_array[@]}" >> median.out
+            done
+
+            dynamic_fps_workers=$(calculate_median "${fps_workers[@]}")
+            dynamic_fps_basic=$(calculate_median "${fps_basic[@]}")
+            dynamic_ms_workers=$(calculate_median "${ms_workers[@]}")
+            dynamic_ms_basic=$(calculate_median "${ms_basic[@]}")
+            expected_values_artifact="{\"basicFps\":${dynamic_fps_basic},\"basicMs\":${dynamic_ms_basic},\"workersFps\":${dynamic_fps_workers},\"workersMs\":${dynamic_ms_workers}}"
+            echo "${expected_values_artifact}" > "${PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT}"
         fi
     fi
+}
 
-    if [[ "$PALINDROME_BENCH_RECALCULATE_MEDIAN" == true ]] || [[ ! -f "$PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT" ]]; then
-        for i in $(seq 1 $PALINDROME_BENCH_MEDIAN_ITERATIONS); do
-            result_array=($(launch_benchmark_and_get_results))
-            fps_workers+=("${result_array[0]}")
-            ms_workers+=("${result_array[1]}")
-            fps_basic+=("${result_array[2]}")
-            ms_basic+=("${result_array[3]}")
-            echo "${result_array[@]}" >> median.out
-        done
+eval_benchmark_run_and_display_results(){
+    declare -a result_array
+    local fps_basic_version
+    local fps_workers_version
+    local ms_basic_version
+    local ms_workers_version
+    local test_failed
+    local test_message
 
-        dynamic_fps_workers=$(calculate_median "${fps_workers[@]}")
-        dynamic_fps_basic=$(calculate_median "${fps_basic[@]}")
-        dynamic_ms_workers=$(calculate_median "${ms_workers[@]}")
-        dynamic_ms_basic=$(calculate_median "${ms_basic[@]}")
-        expected_values_artifact="{\"basicFps\":$dynamic_fps_basic,\"basicMs\":$dynamic_ms_basic,\"workersFps\":$dynamic_fps_workers,\"workersMs\":$dynamic_ms_workers}"
-        echo "${expected_values_artifact}" > "$PALINDROME_BENCH_MEDIAN_VALUES_OUTPUT"
+    # Run benchmark
+    result_array=($(launch_benchmark_and_get_results))
+
+    fps_basic_version="${result_array[0]}"
+    fps_workers_version="${result_array[1]}"
+    ms_basic_version="${result_array[2]}"
+    ms_workers_version="${result_array[3]}"
+
+    # Tests and assertions
+    test_failed=false
+    test_message="[Basic version] should respect minimum FPS values."
+    if compare_float "${fps_basic_version}" "${dynamic_fps_basic}"; then
+        echo -e "\n✕ ${test_message}"
+        echo -e "[ERROR] Basic version FPS is too low"
+        echo "Found: ${fps_basic_version}. Expected: greater than ${dynamic_fps_basic}"
+        test_failed=true
+    else
+        echo -e "\n✓ ${test_message}"
     fi
-fi
 
-# Run benchmark
-result_array=($(launch_benchmark_and_get_results))
-fps_basic_version=${result_array[0]}
-fps_workers_version=${result_array[1]}
-ms_basic_version=${result_array[2]}
-ms_workers_version=${result_array[3]}
+    test_message="[Workers version] should respect minimum FPS values."
+    if compare_float "${fps_workers_version}" "${dynamic_fps_workers}"; then
+        echo -e "\n✕ ${test_message}"
+        echo -e "[ERROR] Web workers version FPS is too low"
+        echo "Found: ${fps_workers_version}. Expected: greater than ${dynamic_fps_workers}"
+        test_failed=true
+    else
+        echo -e "\n✓ ${test_message}"
+    fi
 
-# Tests and assertions
-test_failed=false
-test_message="[Basic version] should respect minimum FPS values."
-if compare_float "$fps_basic_version" "$dynamic_fps_basic"; then
-    echo -e "\n✕ $test_message"
-    echo -e "[ERROR] Basic version FPS is too low"
-    echo "Found: $fps_basic_version. Expected: greater than $dynamic_fps_basic"
-    test_failed=true
-else
-    echo -e "\n✓ $test_message"
-fi
+    test_message="[Basic version] should respect maximum MS values."
+    if ! compare_float "${ms_basic_version}" "${dynamic_ms_basic}"; then
+        echo -e "\n✕ ${test_message}"
+        echo -e "[ERROR] Basic version ms needed to render a frame is too high"
+        echo "Found: ${ms_basic_version}. Expected: less than ${dynamic_ms_basic}"
+        test_failed=true
+    else
+        echo -e "\n✓ ${test_message}"
+    fi
 
-test_message="[Workers version] should respect minimum FPS values."
-if compare_float "$fps_workers_version" "$dynamic_fps_workers"; then
-    echo -e "\n✕ $test_message"
-    echo -e "[ERROR] Web workers version FPS is too low"
-    echo "Found: $fps_workers_version. Expected: greater than $dynamic_fps_workers"
-    test_failed=true
-else
-    echo -e "\n✓ $test_message"
-fi
+    test_message="[Workers version] should respect maximum MS values."
+    if ! compare_float "${ms_workers_version}" "${dynamic_ms_workers}"; then
+        echo -e "\n✕ ${test_message}"
+        echo -e "[ERROR] Web workers ms needed to render a frame is too high"
+        echo "Found: ${ms_workers_version}. Expected: less than ${dynamic_ms_workers}" 
+        test_failed=true
+    else
+        echo -e "\n✓ ${test_message}"
+    fi
 
-test_message="[Basic version] should respect maximum MS values."
-if ! compare_float "$ms_basic_version" "$dynamic_ms_basic"; then
-    echo -e "\n✕ $test_message"
-    echo -e "[ERROR] Basic version ms needed to render a frame is too high"
-    echo "Found: $ms_basic_version. Expected: less than $dynamic_ms_basic"
-    test_failed=true
-else
-    echo -e "\n✓ $test_message"
-fi
+    if [[ "${test_failed}" == true ]]; then
+        exit 1
+    fi
+    echo -e "\n✓ All tests passed."
+    exit 0
+}
 
-test_message="[Workers version] should respect maximum MS values."
-if ! compare_float "$ms_workers_version" "$dynamic_ms_workers"; then
-    echo -e "\n✕ $test_message"
-    echo -e "[ERROR] Web workers ms needed to render a frame is too high"
-    echo "Found: $ms_workers_version. Expected: less than $dynamic_ms_workers" 
-    test_failed=true
-else
-    echo -e "\n✓ $test_message"
-fi
+eval_benchmark_wrapper(){
+    # remove: outputFile="${HOME}/Downloads/${PALINDROME_BENCH_OUTPUT}.results"
+    local dynamic_fps_workers
+    local dynamic_ms_workers
+    local dynamic_fps_basic
+    local dynamic_ms_basic
+    local remote_values
+    local median_values
+    local expected_values_artifact
+    local test_failed
+    local test_message
 
-if [[ "$test_failed" == true ]]; then
-    exit 1
-fi
-echo -e "\n✓ All tests passed."
-exit 0
+    declare -a fps_workers
+    declare -a ms_workers
+    declare -a fps_basic
+    declare -a ms_basic
+    declare -a result_array
+
+    eval_benchmark_get_median
+    eval_benchmark_run_and_display_results
+}
--- benchmark.sh
+++ benchmark.sh
@@ -4,146 +4,204 @@ source ./benchmark.env.sh
 # Trap SIGINT to clean up before exiting
 trap cleanup INT
 
-cleanup() {
+# inherit: output_file
+simple_benchmark_exit_cleanup() {
+    local firefox_profile
+
     echo -e "\033[1m[INFO] Cleaning up and exiting...\n\033[0m"
     pkill -f firefox
     pkill -f parcel
     firefox_profile=~/.mozilla/firefox/profiles.ini
-    if [[ -n "$GITLAB_CI" ]]; then
-        if [[ -f "$firefox_profile" ]]; then
-            rm "$firefox_profile"
+    if [[ -n "${GITLAB_CI}" ]]; then
+        if [[ -f "${firefox_profile}" ]]; then
+            rm "${firefox_profile}"
         fi
     fi
-    if [[ -f "$output_file" ]]; then
-        rm "$output_file"
+    if [[ -f "${output_file}" ]]; then
+        rm "${output_file}"
     fi
-    if [[ -f "$output_file.txt" ]]; then
-        rm "$output_file.txt"
+    if [[ -f "${output_file}.txt" ]]; then
+        rm "${output_file}.txt"
     fi
     exit 0
 }
 
-# Set the base path and construct the output file path
-mkdir -p "${HOME}/${PALINDROME_BENCH_DL_DIRECTORY}"
-output_file="${HOME}/${PALINDROME_BENCH_DL_DIRECTORY}/${PALINDROME_BENCH_OUTPUT}"
-
 # Remove cache and existing output file
-rm -rf .cache
-if [[ -f "$output_file" ]]; then
-    rm "$output_file"
-fi
-if [[ -f "$output_file.txt" ]]; then
-    rm "$output_file.txt"
-fi
-
-if [[ "$PALINDROME_BENCH_WEBSERVER" == true ]]; then
-    # Kill processes on port 1234 and start Parcel
-    fuser -k 1234/tcp
-    yarn parcel dev/index.html &
-    # parcel_pid=$!
-
-    # Wait for Parcel to start
-    sleep 30
-    url="http://localhost:1234"
-else
-    yarn parcel build dev/index.html --public-url ./ --no-cache
-    url="./dist/index.html"
-fi
-
-# Launch browser
-Xvfb :1 -screen 0 1024x768x16 &
-export DISPLAY=:1
-
-if [[ "$PALINDROME_BENCH_HEADLESS" == false ]]; then
-    if [[ "$PALINDROME_BENCH_GPU" == true ]]; then
-        firefox -url $url &
-    else
-        google-chrome --disable-gpu --no-sandbox $url&
+# Inherit : $output_file
+simple_benchmark_entry_cleanup(){
+    rm -rf .cache
+    if [[ -f "${output_file}" ]]; then
+        rm "${output_file}"
     fi
-else
-    if [[ "$PALINDROME_BENCH_GPU" == true ]]; then
-        firefox --headless -url $url &
+    if [[ -f "${output_file}.txt" ]]; then
+        rm "${output_file}.txt"
+    fi
+}
+
+# Set benchmark endpoint
+# Inherit: $url
+simple_benchmark_set_endpoint(){
+    if [[ "${PALINDROME_BENCH_WEBSERVER}" == true ]]; then
+        # Kill processes on port 1234 and start Parcel
+        fuser -k 1234/tcp
+        yarn parcel dev/index.html &
+        # remove: parcel_pid=$!
+        # Wait for Parcel to start
+        sleep 30
+        url="http://localhost:1234"
     else
-        if [[ -n "$GITLAB_CI" ]]; then
-            google-chrome --no-sandbox &
-            chrome_pid=$!
-            sleep 10
-            kill -9 "$chrome_pid"
-            sleep 30
-            google-chrome --disable-gpu --no-sandbox $url&
+        yarn parcel build dev/index.html --public-url ./ --no-cache
+        url="./dist/index.html"
+    fi
+}
+
+# Execute browsers depending on ENV values
+# Inherit: $url
+simple_benchmark_execute_browsers(){
+    local chrome_pid
+
+    # Launch browser
+    Xvfb :1 -screen 0 1024x768x16 &
+    export DISPLAY=:1
+
+    # Execute browsers depending on ENV values
+    if [[ "${PALINDROME_BENCH_HEADLESS}" == false ]]; then
+        if [[ "${PALINDROME_BENCH_GPU}" == true ]]; then
+            firefox -url $url &
         else
-            firefox --headless -url $url &
-        fi;
+            # review: ${url}& or ${url} &
+            google-chrome --disable-gpu --no-sandbox ${url}&
+        fi
+    else
+        if [[ "${PALINDROME_BENCH_GPU}" == true ]]; then
+            firefox --headless -url ${url} &
+        else
+            if [[ -n "${GITLAB_CI}" ]]; then
+                google-chrome --no-sandbox &
+                chrome_pid=$!
+                sleep 10
+                kill -9 "${chrome_pid}"
+                sleep 30
+                # review: ${url}& or ${url} &
+                google-chrome --disable-gpu --no-sandbox ${url}&
+            else
+                firefox --headless -url ${url} &
+            fi;
+        fi
     fi
-fi
+}
 
-# Calculate sleep duration based on BENCHMARK_DURATION or default to 60 seconds
-if declare -p | grep -q "PALINDROME_BENCH_DURATION"; then
-    sleep_duration=$((PALINDROME_BENCH_DURATION * 60 * 2))
-else
-    sleep_duration=$((60))
-fi
+# Calculate sleep duration based on BENCHMARK_DURATION 
+# or default to 60 seconds
+# Inherit: $sleep_duration
+simple_benchmark_set_sleep(){
+    if declare -p | grep -q "PALINDROME_BENCH_DURATION"; then
+        sleep_duration=$(( PALINDROME_BENCH_DURATION * 60 * 2 ))
+    else
+        # review: sleep_duration=$((60))
+        sleep_duration=60
+    fi
+}
 
 # Display benchmark total duration
-echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Benchmark started. Total duration: $sleep_duration seconds.\n\033[0m"
-while [[ ! -f "$output_file" ]] && [[ ! -f "$output_file.txt" ]]; do
-    sleep 0.1
-done
+# inherit: $output_file, $sleep_duration
+simple_benchmark_display_duration(){
+    echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Benchmark started. Total duration: ${sleep_duration} seconds.\n\033[0m"
+    while [[ ! -f "${output_file}" ]] && [[ ! -f "${output_file}.txt" ]]; do
+        sleep 0.1
+    done
+}
 
-# Wait for file to be downloaded
-sleep 1
+# Display benchmark results
+# Inherit: $output_file, $sleep_duration
+simple_benchmark_display_results(){
+    local cpu_info
+    local ram_capacity
+    local file_content
+    local fps_basic_version
+    local fps_workers_version
+    local ms_basic_version
+    local ms_workers_version
+    local data_structure
 
-# Display benchmark finish message
-echo -e "\033[1m\n[INFO] Benchmark done, displaying results.\n\033[0m"
+    # Display benchmark finish message
+    echo -e "\033[1m\n[INFO] Benchmark done, displaying results.\n\033[0m"
 
-# Capture system information
-cpu_info=$(lscpu)
-ram_capacity=$(free -h | awk '/^Mem:/ {print $2}')
-ram_capacity_formatted="RAM: $ram_capacity"
+    # Capture system information
+    cpu_info=$(lscpu)
+    ram_capacity=$(free -h | awk '/^Mem:/ {print $2}')
+    ram_capacity="RAM: ${ram_capacity}"
 
-# Append benchmark context and Palindrome.js config to the output file
-if [[ "$PALINDROME_BENCH_GPU" == true ]]; then
-    file_content=$(cat "$output_file")
-else
-    if [[ -n "$GITLAB_CI" ]]; then
-        file_content=$(cat "$output_file.txt")
+    # Review: Append benchmark context and Palindrome.js config to the output file
+    if [[ "${PALINDROME_BENCH_GPU}" == true ]]; then
+        file_content=$(cat "${output_file}")
     else
-        file_content=$(cat "$output_file")
+        if [[ -n "$GITLAB_CI" ]]; then
+            file_content=$(cat "${output_file}.txt")
+        else
+            file_content=$(cat "${output_file}")
+        fi
     fi
-fi
 
-# Getting JSON Data from results file
-fps_basic_version=$(echo "$file_content" | jq -r '.Basic_version_results."Average FPS rendered"')
-fps_workers_version=$(echo "$file_content" | jq -r '."Web workers_version_results"."Average FPS rendered"')
+    # Getting JSON Data from results file
+    fps_basic_version=$(echo "${file_content}" | jq -r '.Basic_version_results."Average FPS rendered"')
+    fps_workers_version=$(echo "${file_content}" | jq -r '."Web workers_version_results"."Average FPS rendered"')
+    ms_basic_version=$(echo "${file_content}" | jq -r '.Basic_version_results."Average Milliseconds needed to render a frame"')
+    ms_workers_version=$(echo "${file_content}" | jq -r '."Web workers_version_results"."Average Milliseconds needed to render a frame"')
+    data_structure=$(echo "${file_content}" | jq -r '.palindrome_config')
 
-ms_basic_version=$(echo "$file_content" | jq -r '.Basic_version_results."Average Milliseconds needed to render a frame"')
-ms_workers_version=$(echo "$file_content" | jq -r '."Web workers_version_results"."Average Milliseconds needed to render a frame"')
+    # Display output files path
+    echo -e "\033[1m\n[INFO] Output files:\n- ${output_file}.context \n- ${output_file}.results \n- ${output_file}.data\033[0m"
+    # echo -e "-------$currentDateTime Benchmark context:\n" > "$output_file.context"
+    echo -e "------- Benchmark context:\n" > "${output_file}.context"
+    echo "${data_structure}" > "${output_file}.data"
+    echo "${cpu_info}" >> "${output_file}.context"
+    echo "${ram_capacity}" >> "${output_file}.context"
+    echo "${file_content}" > "${output_file}.results_"
+    jq 'del(.palindrome_config)' ${output_file}.results_ > ${output_file}.results
+    rm ${output_file}.results_
+    cat "${output_file}.context"
+    echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Palindrome.js benchmark results:\n\033[0m"
+    #cat "$output_file.results"
+    echo -e "\n-------------------------------------------------------------------------------------"
+    echo "                                  Basic version                   Web workes version"
+    echo "-------------------------------------------------------------------------------------"
+    echo "FPS (Frames per second)      |    ${fps_basic_version}                           ${fps_workers_version}"
+    echo "MS needed to render a frame  |    ${ms_basic_version}                           ${ms_workers_version}"
+    echo "-------------------------------------------------------------------------------------"
+    echo "Total duration (seconds)     |    ${sleep_duration}"
+    echo "-------------------------------------------------------------------------------------"
 
-data_structure=$(echo "$file_content" | jq -r '.palindrome_config')
+    #echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Benchmark total time $sleep_duration seconds.\n\033[0m" 
 
-# Display output files path
-echo -e "\033[1m\n[INFO] Output files:\n- $output_file.context \n- $output_file.results \n- $output_file.data\033[0m"
+}
 
-echo -e "-------$currentDateTime Benchmark context:\n" > "$output_file.context"
-echo "$data_structure" > "$output_file.data"
-echo "$cpu_info" >> "$output_file.context"
-echo "$ram_capacity_formatted" >> "$output_file.context"
-echo "$file_content" > "$output_file.results_"
-jq 'del(.palindrome_config)' $output_file.results_ > $output_file.results
-rm $output_file.results_
-cat "$output_file.context"
-echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Palindrome.js benchmark results:\n\033[0m"
-#cat "$output_file.results"
-echo -e "\n-------------------------------------------------------------------------------------"
-echo "                                  Basic version                   Web workes version"
-echo "-------------------------------------------------------------------------------------"
-echo "FPS (Frames per second)      |    $fps_basic_version                           $fps_workers_version"
-echo "MS needed to render a frame  |    $ms_basic_version                           $ms_workers_version"
-echo "-------------------------------------------------------------------------------------"
-echo "Total duration (seconds)     |    $sleep_duration"
-echo "-------------------------------------------------------------------------------------"
+# Wrap the simple benchmark functions
+simple_benchmark_wrapper(){
+    local output_file
+    local url
+    local sleep_duration
 
-#echo -e "\033[1m[INFO] $(date '+%Y-%m-%dT%H:%M:%S') Benchmark total time $sleep_duration seconds.\n\033[0m" 
+    # Set the base path and construct the output file path
+    mkdir -p "${HOME}/${PALINDROME_BENCH_DL_DIRECTORY}"
+    output_file="${HOME}/${PALINDROME_BENCH_DL_DIRECTORY}/${PALINDROME_BENCH_OUTPUT}"
 
-# Clean up and exit
-cleanup
+    simple_benchmark_entry_cleanup
+
+    simple_benchmark_set_endpoint
+
+    simple_benchmark_execute_browsers
+
+    simple_benchmark_set_sleep
+
+    simple_benchmark_display_duration
+
+    # Wait for file to be downloaded
+    sleep 1
+
+    simple_benchmark_display_results
+
+    # Clean up and exit
+    simple_benchmark_exit_cleanup
+}
+
