fc0fead61da0c643434bc925fd17b17e289e005e
jonriv <jonathan.rivalan@smile.fr>
2024-04-19 11:41:17 +0200

    add: options to select between visual or headless benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -7,12 +7,25 @@ source ./benchmark/benchmark-dependencies.sh
 source ./benchmark/benchmark.env.sh
 
 function main(){
-    local option=${1:-classic}
-    if [[ "${option}" == "classic" ]]; then
+    declare -g BENCHMARK_TYPE
+    declare -g BENCHMARK_MODE
+
+    BENCHMARK_TYPE=${1:-classic}
+    BENCHMARK_MODE=${2:-headless}
+
+    # set the HEADLESS env value
+    if [[ "${BENCHMARK_MODE}" == "headless" ]]; then
+        PALINDROME_BENCH_HEADLESS=true
+    elif [[ "${BENCHMARK_MODE}" == "visual" ]]; then
+        PALINDROME_BENCH_HEADLESS=false
+    fi
+
+    # execute the benchmark depending on selected type
+    if [[ "${BENCHMARK_TYPE}" == "classic" ]]; then
         source ./benchmark/classic-benchmark.sh
-    elif [[ "${option}" == "evaluation" ]]; then
+    elif [[ "${BENCHMARK_TYPE}" == "evaluation" ]]; then
         source ./benchmark/eval-benchmark.sh
     fi
 }
 
-main "${@}"
+main "${@-}"
