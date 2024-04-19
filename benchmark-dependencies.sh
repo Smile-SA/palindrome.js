e2bd0489d6c11c67239119d6d690957594c78b35
jonriv <jonathan.rivalan@smile.fr>
2024-04-19 11:40:26 +0200

    add: sudo support for non-gitlab-ci; improved conditional support

--- benchmark-dependencies.sh
+++ benchmark-dependencies.sh
@@ -3,52 +3,59 @@
 # Set up benchmark parameters
 source ./benchmark.env.sh
 
-# Update the package manager
-apt update -y
+# Set sudo prefix depending on situation
+declare -g cmd_prefix
+if [[ -n "${GITLAB_CI}" ]]; then
+    cmd_prefix=""
+else
+    cmd_prefix="sudo"
+fi
 
-# Install wget
-apt install wget -y
+# Update the package manager
+${cmd_prefix} apt update -y
+
+# Install wget if not already installed
+if [[ -z "$(command -v wget)" ]]; then
+    ${cmd_prefix} apt install -y wget
+    jq --version
+fi
 
 # Install google-chrome
 if [[ "${PALINDROME_BENCH_BROWSER}" == 'chromium' ]]; then
     if [ -z "$(command -v chromium)" ]; then
-        if [[ -n "${GITLAB_CI}" ]]; then
-            export TZ=Europe/Paris 
-            ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-            apt install chromium-browser -y
-            snap install chromium
-        else
-            export TZ=Europe/Paris 
-            ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-            sudo apt install chromium-browser -y
-            sudo snap install chromium
-        fi
+        export TZ=Europe/Paris 
+        ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+        ${cmd_prefix} apt install chromium-browser -y
+        ${cmd_prefix} snap install chromium
     fi
 fi
 
 # Install fuser command
-apt install psmisc -y
+if [[ -z "$(command -v fuser)" ]]; then
+    ${cmd_prefix} apt install psmisc -y
+fi
 
 # Install xvfb
-apt install xvfb -y
+if [[ -z "$(command -v xvfb-run)" ]]; then
+    ${cmd_prefix} apt install xvfb -y
+fi
 
 # Install jq if not already installed
 if [[ -z "$(command -v jq)" ]]; then
-    apt install -y jq
+    ${cmd_prefix} apt install -y jq
     jq --version
 fi
 
 # Install curl if not already installed
 if [[ -z "$(command -v curl)" ]]; then
-    apt install -y sudo curl
+    ${cmd_prefix} apt install -y sudo curl
     curl --version
 fi
-
 
 # Install Node.js LTS version if not already installed
 if [[ -z "$(command -v node)" ]]; then
     curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-    apt install -y nodejs
+    ${cmd_prefix} apt install -y nodejs
     node -v
 fi
 
@@ -56,10 +63,10 @@ fi
 if [[ -z "$(command -v yarn)" ]]; then
     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    apt update && sudo apt install -y yarn
+    ${cmd_prefix} apt update && sudo apt install -y yarn
 fi
 
 # Install Firefox if not already installed
 if [[ -z "$(command -v firefox)" ]]; then
-    apt install -y firefox xvfb
+    ${cmd_prefix} apt install -y firefox
 fi
